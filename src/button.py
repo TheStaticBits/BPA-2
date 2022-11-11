5c0af4c011f10548e46d704ddffe4ce311a5bd59
StaticBits <puppy2655@gmail.com>
2022-11-11 16:40:24 -0500

    ui files added

--- src/button.py
+++ src/button.py
@@ -4,7 +4,8 @@ import logging
 import utility as util
 
 class Button:
-    def __init__(self, pos, ):
+    def __init__(self, pos):
         self.log = logging.getLogger(__name__)
 
     def update(self, window):
+        pass
